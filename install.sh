4abc286547adbde94bf02e16382b9112bce32a94
Fabien Crassat <fabien@crassat.com>
2018-09-27 23:52:05 +0200

    Finish bash

--- install.sh
+++ install.sh
@@ -2,8 +2,12 @@
 
 #### FUNCTIONS ####
 
-echoTitle() {
+echoHeader() {
+  echo ============================
+  echo $1
   echo
+}
+echoTitle() {
   echo ----------------------------
   echo $1
   echo
@@ -15,6 +19,16 @@ removeYarnLock() {
   rm -f yarn-lock.json
 }
 getLodashVersion() {
+  echoTitle "$2"
+
+  if [ $1 = "yarn" ]; then
+    yarn list --pattern lodash
+  elif [ $1 = "npm" ]; then
+    npm list lodash
+  else
+    echo "wrong args" && exit 1
+  fi
+
   file=node_modules/lodash/package.json
   while read line; do
     if [[ $line =~ \"version\": ]] ; then echo $line; fi
@@ -29,40 +43,46 @@ removeYarnLock
 cp assets/package-init.json package.json
 
 # YARN installation
+echo 
+echoHeader "YARN installation"
 # NPM does not know YARN but the opposite is not true, so we begin with it.
-echoTitle "Install with YARN the lodash version 4.16.2"
+echoTitle "Install the lodash version 4.16.2"
 removePackageLock
 yarn install
-echoTitle "Check the versions = 4.16.2"
-yarn list --pattern lodash
-getLodashVersion
+echo 
+getLodashVersion "yarn" "The version MUST BE 4.16.2"
 
 # NPM installation
-echoTitle "Install with NPM the lodash version 4.16.2"
+echo 
+echoHeader "NPM installation"
+echoTitle "Install the lodash version 4.16.2"
 npm install
-echoTitle "Check the versions = 4.16.2"
-npm list lodash
-getLodashVersion
+echo 
+getLodashVersion "npm" "The version MUST BE 4.16.2"
 
+# Installation behavior
+echo 
+echoHeader "Installation behavior"
 # Change package.json dependencies
 echoTitle "Change to install version ~4.16.2 (patch can change)"
 cp assets/package-newDep.json package.json
 
 # YARN installation
+echoHeader "YARN installation"
 # It will take the last pach available, at this time 4.16.6
 # So it is looking at package.json, not yarn.lock
-echoTitle "Install with YARN the lodash version ~4.16.2"
+echoTitle "Install the lodash version ~4.16.2"
 yarn install
-echoTitle "Check the versions = 4.16.6 (should be)"
-yarn list --pattern lodash
-getLodashVersion
+echo 
+getLodashVersion "yarn" "The version SHOULD BE 4.16.6"
 
 # NPM installation
+echo 
+echoHeader "NPM installation"
 # It will take the package-lock.json version,so 4.16.2
-echoTitle "Install with NPM the lodash version ~4.16.2"
+echoTitle "Install the lodash version ~4.16.2"
 npm install
-echoTitle "Check the versions = 4.16.2"
-npm list lodash
-getLodashVersion
+echo 
+getLodashVersion "npm" "The version MUST BE 4.16.2"
 
 exit 0
